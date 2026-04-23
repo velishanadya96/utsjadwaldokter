fc61e4f47de23a00603a7260e47552e4d35df8c3
velishanadya96 <velishanadya96@gmail.com>
2026-04-23 16:00:19 +0700

    updateconfig

--- api/config.php
+++ api/config.php
@@ -1,12 +1,30 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "klinik_db";
+// Data dari TiDB Cloud
+$host = 'gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com';
+$port = 4000;
+$user = '2h7AMb9FTB6BmYe.root';
+$pass = '29xnSbI4kHNOZnPN';
+$db   = 'klinik-db';
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+// Inisialisasi mysqli
+$koneksi = mysqli_init();
 
-if (!$conn) {
-    die("Koneksi gagal: " . mysqli_connect_error());
+// Menambahkan pengaturan SSL (Wajib untuk TiDB Serverless)
+mysqli_ssl_set($koneksi, NULL, NULL, NULL, NULL, NULL);
+
+// Melakukan koneksi
+$real_connect = mysqli_real_connect(
+    $koneksi, 
+    $host, 
+    $user, 
+    $pass, 
+    $db, 
+    $port, 
+    NULL, 
+    MYSQLI_CLIENT_SSL
+);
+
+if (!$real_connect) {
+    die("Koneksi ke TiDB Cloud gagal: " . mysqli_connect_error());
 }
 ?>
