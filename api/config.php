b1ef39b5a58ab58475e30da49d22b859701607c2
velishanadya96 <velishanadya96@gmail.com>
2026-04-27 21:11:29 +0700

    empat

--- api/config.php
+++ api/config.php
@@ -1,21 +1,15 @@
 <?php
-
 $host = 'gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com';
 $port = 4000;
 $user = '2h7AMb9FTB6BmYe.root';
 $pass = '29xnSbI4kHNOZnPN';
 $db   = 'klinik-db';
 
-$conn = mysqli_init();
+$conn = mysqli_init(); // Pakai $conn
 mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
-$real_connect = mysqli_real_connect($conn, $host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL);
 
-// Menambahkan pengaturan SSL (Wajib untuk TiDB Serverless)
-mysqli_ssl_set($koneksi, NULL, NULL, NULL, NULL, NULL);
-
-// Melakukan koneksi
 $real_connect = mysqli_real_connect(
-    $koneksi, 
+    $conn, // Jangan pakai $koneksi
     $host, 
     $user, 
     $pass, 
